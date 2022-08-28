f33109d7f211c2960d6a7fb78af54a50e382b31b
SvetlanaShin <susperson@gmail.com>
2022-08-28 14:03:26 +0600

    Переменные ч.2. Начальный коммит

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1
+        int intNumber = 843;
+        byte byteNumber = 17;
+        short shortNumber = 19245;
+        long longNumber = 9434643516844L;
+        float floatNumber = (float) 15.1002;
+        double doubleNumber = 144.13654888888;
+        System.out.println("Значение переменной intNumber с типом int равно " + intNumber);
+        System.out.println("Значение переменной byteNumber с типом byte равно " + byteNumber);
+        System.out.println("Значение переменной shortNumber с типом short равно " + shortNumber);
+        System.out.println("Значение переменной longNumber с типом long равно " + longNumber);
+        System.out.println("Значение переменной floatNumber с типом float равно " + floatNumber);
+        System.out.println("Значение переменной doubleNumber с типом double равно " + doubleNumber);
+    }
+}
